84bab43577e7e017e17822bc32abadcdd6e6597b
Matthieu Biville <mattbvl0104@gmail.com>
2025-10-19 08:35:44 +0200

    add config for export and good functionning

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    trailingSlash: true,
     images: {
         domains: ['static-cdn.jtvnw.net'],
         unoptimized: true,
